6713341769bdf16f8640b46938e17923b937ead8
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2023-01-29 11:50:23 +0530

    Adding modules for the snake game

---
+++ The Snake Game/scoreboard.py
@@ -0,0 +1,25 @@
+from turtle import Turtle
+
+class Score(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("white")
+        self.penup()
+        self.goto(0,270)
+        self.update_score()
+        self.hideturtle()
+        
+    def update_score(self):
+        self.write(f"Score: {self.score}", align="center",font=("Courier", 18, "normal"))
+        
+    def game_over(self):
+        self.goto(0,0)
+        self.write("GAME OVER", align = "center",font=("Courier", 18, "normal"))
+        
+    def increse_score(self):
+        self.score += 1
+        self.clear()
+        self.update_score()
+        
+        
