9b4511433ac080f5b7940b3c3cb2e0fe79ae75e6
Udit Thacker <65917694+UditThakkar@users.noreply.github.com>
2023-02-02 20:20:01 +0530

    Take high score from local storage for better experience

--- The Snake Game/scoreboard.py
+++ The Snake Game/scoreboard.py
@@ -4,7 +4,9 @@ class Score(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.high_score = 0
+        # self.high_score = 0
+        with open("score.txt") as data:
+            self.high_score = int(data.read())
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -22,6 +24,8 @@ class Score(Turtle):
     def reset(self):
         if self.score > self.high_score:
             self.high_score = self.score
+            with open("score.txt", "W") as data:
+                data.write(f"{self.high_score}")
         self.score = 0
         self.update_score()
         
